7ac22d8ea2e595c92aefcc1f142366c764d143c8
wizkiz <szy.majorek@gmail.com>
2019-11-15 00:49:29 +0100

    Lab3 ex3 fixed

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -7,7 +7,8 @@ export default class MyComponent extends React.Component {
         this.state = {
             employees: null,
 			isLoading: false,
-			isSaving: false
+			isSaving: false,
+			isFormActive: false
         }
     }
 
@@ -66,23 +67,24 @@ export default class MyComponent extends React.Component {
 			})
 		})
 			.then(response => response.json())
-			.then(this.setState({isSaving: false}))
-			.then(this.reloadEmployees());
+			.then(() => this.setState({isSaving: false, isFormActive: false}))
+			.then(() => this.reloadEmployees());
     }
 
     handlerFormReset = (event) =>{
 
         event.preventDefault();
         this.setState({isFormActive: false});
-    }
+	}
 
     render() {
-
+		console.log("render");
         const addEmployeeButton = (
             <p><button onClick={this.handlerAddEmployeeButton}>Add employee</button></p>
         )
 
         const addEmployeeForm = (
+			this.state.isSaving ? <p>"Saving..."</p> :
             <div>
                 <form onSubmit={this.handlerFormSubmit} onReset={this.handlerFormReset}>
                     <h1>Add an employee</h1>
@@ -106,25 +108,26 @@ export default class MyComponent extends React.Component {
                         Email: 
                         <input name="email"/>
                     </label></p>
-                    <p><input type="submit"/></p>
-                    <p><input type="reset"/></p>
+                    <p><input type="submit" value="Submit"/> <input type="reset" value="Cancel"/></p>
                 </form>
             </div>
-        )
+		)
 
-		if(this.state.isSaving) {
-			return <p>Saving...</p>
-		}
+		// if(this.state.isSaving) {
+		// 	return <p>Saving...</p>
+		// }
 
         if(this.state.isLoading) {
-            return <p>Loading...</p>
+        	return <p>Loading...</p>
 		}
 
         if(this.state.employees) {
+
+
             return (
                 <div>
                     <p>Data loaded, {this.state.employees.length} employees fetched</p>
-                    {this.state.isFormActive ? addEmployeeForm : addEmployeeButton}
+					{this.state.isFormActive ? addEmployeeForm : addEmployeeButton}
                 </div>
             )
         }
